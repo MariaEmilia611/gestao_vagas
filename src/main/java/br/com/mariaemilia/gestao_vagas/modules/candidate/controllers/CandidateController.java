a51c020d37008e55c0ccc5ccec9883f7afaf94d0
MariaEmilia611 <maria945duarteferreira@outlook.com>
2025-10-01 17:12:59 -0300

    Add nas dependencias o repository

--- src/main/java/br/com/mariaemilia/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/mariaemilia/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,21 +1,26 @@
 package br.com.mariaemilia.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mariaemilia.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.mariaemilia.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
     public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-     System.out.println("Candidato");
-     System.out.println(candidateEntity.getEmail());
+        this.candidateRepository.save(candidateEntity);
+     
     }
     
 }
