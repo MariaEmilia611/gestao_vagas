090e3c5d8c0b8611ec2649b98461fe334090f9e6
MariaEmilia611 <maria945duarteferreira@outlook.com>
2025-11-17 15:18:01 -0300

    Condições de controle de candidatos

--- src/main/java/br/com/mariaemilia/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/mariaemilia/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -6,30 +6,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import br.com.mariaemilia.gestao_vagas.exceptions.UserFoundException;
 import br.com.mariaemilia.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.mariaemilia.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.mariaemilia.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
-    @Autowired
-    private CandidateRepository candidateRepository;
+  @Autowired
+  private CreateCandidateUseCase createCandidateUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
-            // 1. Salve a entidade E capture o resultado na variável 'result'
-            var result = this.candidateRepository.save(candidateEntity);
-
-            // 2. Retorne a variável 'result'. O Spring irá convertê-la para JSON automaticamente.
-            return ResponseEntity.ok().body(result);
+          var result = createCandidateUseCase.execute(candidateEntity);
+          return ResponseEntity.ok().body(result);
         } catch (Exception e) {
-            // É uma boa prática retornar o erro caso algo dê errado na validação
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+          return ResponseEntity.badRequest().body(e.getMessage());
+        }  
     }
-    
-}
+  }      
